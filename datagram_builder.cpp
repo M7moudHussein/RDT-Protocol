77e5a1747a60cf31316b9c9f604fabc04a16e7d6
Mahmoud Hussein <m7moud.hussein96@gmail.com>
2018-12-06 18:45:36 +0200

    Add datagram builder
    - Reads the specified number of packets from file
    - maps the read data to datagrams

--- datagram_builder.cpp
+++ datagram_builder.cpp
@@ -1,5 +1,51 @@
-//
-// Created by mahmoud on 12/6/18.
-//
-
 #include "datagram_builder.h"
+#include <sys/stat.h>
+#include <algorithm>
+
+datagram_builder::datagram_builder(std::string absolute_path, int queue_size) {
+    struct stat statbuf;
+    if (stat(absolute_path.c_str(), &statbuf) == -1) {
+        //TODO handle error
+    }
+    data_not_read = statbuf.st_size;
+    buffer = new char[BUFFER_SIZE];
+
+    FILE *fp = fopen(absolute_path.c_str(), "rb");
+
+    if (fp == NULL) {
+        //TODO handle error
+    }
+}
+
+datagram *datagram_builder::get_next_datagram() {
+    if (data_not_read > 0 && datagram_queue.empty()) {
+        size_t read_length = fread(buffer, sizeof(char), BUFFER_SIZE, fp);
+        data_not_read -= read_length;
+
+        if (data_not_read <= 0) {
+            fclose(fp);
+            delete[] buffer;
+            delete fp;
+        } else if (read_length == -1) {
+            //TODO handle error
+        }
+
+        size_t packet_start_index = 0;
+        while (read_length > 0) {
+            std::string packet_data = std::string(buffer + packet_start_index,
+                                                  std::min(read_length, (size_t) PACKET_SIZE));
+
+            datagram_queue.push(new datagram(packet_data));
+
+            read_length -= PACKET_SIZE;
+            packet_start_index += PACKET_SIZE;
+        }
+    }
+    datagram *next_datagram = datagram_queue.front();
+    datagram_queue.pop();
+    return next_datagram;
+}
+
+bool datagram_builder::has_next() {
+    return data_not_read > 0;
+}
--- datagram_builder.h
+++ datagram_builder.h
@@ -1,15 +1,27 @@
-//
-// Created by mahmoud on 12/6/18.
-//
-
 #ifndef RDT_PROTOCOL_DATAGRAM_BUILDER_H
 #define RDT_PROTOCOL_DATAGRAM_BUILDER_H
 
 
-class datagram_builder {
-private:
-    int queue_size;
+#include <string>
+#include <queue>
+#include "datagram.h"
 
+class datagram_builder {
+public:
+    datagram_builder(std::string absolute_path, int queue_size);
+
+    datagram *get_next_datagram();
+
+    bool has_next();
+
+private:
+    FILE *fp;
+    int queue_size;
+    size_t data_not_read;
+    std::queue<datagram *> datagram_queue;
+    char *buffer;
+
+#define BUFFER_SIZE PACKET_SIZE * queue_size
 };
 
 
