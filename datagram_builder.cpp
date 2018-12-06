010a133f3af1211b657a4e74bca73449b61009d6
Mahmoud Hussein <m7moud.hussein96@gmail.com>
2018-12-06 16:26:34 +0200

    Add input files

---
+++ datagram_builder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mahmoud on 12/6/18.
+//
+
+#include "datagram_builder.h"
---
+++ datagram_builder.h
@@ -0,0 +1,16 @@
+//
+// Created by mahmoud on 12/6/18.
+//
+
+#ifndef RDT_PROTOCOL_DATAGRAM_BUILDER_H
+#define RDT_PROTOCOL_DATAGRAM_BUILDER_H
+
+
+class datagram_builder {
+private:
+    int queue_size;
+
+};
+
+
+#endif //RDT_PROTOCOL_DATAGRAM_BUILDER_H
