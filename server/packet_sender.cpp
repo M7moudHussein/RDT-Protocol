deaadc64905b675c6f2b3f8a8d19ebdf91500a2c
Mahmoud Hussein <m7moud.hussein96@gmail.com>
2018-12-14 16:30:57 +0200

    Add loss with sequence

--- server/packet_sender.cpp
+++ server/packet_sender.cpp
@@ -1,18 +1,25 @@
-//
-// Created by mahmoud on 12/14/18.
-//
-
 #include "packet_sender.h"
-#include <random>
 #include <functional>
 
 float packet_sender::loss_probability;
 std::minstd_rand0 packet_sender::generator;
 std::uniform_real_distribution<double> packet_sender::distribution;
+loss_mode packet_sender::mode;
+int packet_sender::loss_sequence_index = 0;
+int packet_sender::packet_number = 0;
+std::vector<int> packet_sender::loss_sequence;
+
+void packet_sender::set_mode(loss_mode mode) {
+    packet_sender::mode = mode;
+}
 
 void packet_sender::set_seed(unsigned int seed) {
     packet_sender::generator = std::default_random_engine(seed);
     packet_sender::distribution = std::uniform_real_distribution<double>(0.0, 100.0);
+}
+
+void packet_sender::set_loss_sequence(std::vector<int> sequence) {
+    packet_sender::loss_sequence = sequence;
 }
 
 void packet_sender::set_probability(const float loss_probability) {
@@ -20,10 +27,22 @@ void packet_sender::set_probability(const float loss_probability) {
 }
 
 void packet_sender::send_packet(int server_socket, sockaddr_in client_socket, data_packet *packet) {
-    if (packet_sender::distribution(packet_sender::generator) > loss_probability) {
-        sendto(server_socket, packet->pack().c_str(), packet->pack().length(),
-               MSG_CONFIRM, (const struct sockaddr *) &client_socket,
-               sizeof(client_socket));
+    switch (packet_sender::mode) {
+        case PROBABILITY:
+            if (packet_sender::distribution(packet_sender::generator) > loss_probability) {
+                sendto(server_socket, packet->pack().c_str(), packet->pack().length(),
+                       MSG_CONFIRM, (const struct sockaddr *) &client_socket,
+                       sizeof(client_socket));
+            }
+            break;
+        case SEQUENTIAL:
+            if (packet_number++ % packet_sender::loss_sequence[loss_sequence_index] == 0) {
+                loss_sequence_index = loss_sequence_index++ % packet_sender::loss_sequence.size();
+                sendto(server_socket, packet->pack().c_str(), packet->pack().length(),
+                       MSG_CONFIRM, (const struct sockaddr *) &client_socket,
+                       sizeof(client_socket));
+            }
+            break;
     }
 }
 
--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,3 @@
-//
-// Created by awalid on 12/5/18.
-//
-
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -20,6 +16,7 @@ server::server(server_parser serv_parser) : MAX_WINDOW_SIZE(serv_parser.get_max_
 
     packet_sender::set_seed(serv_parser.get_random_seed());
     packet_sender::set_probability(serv_parser.get_loss_probability());
+    packet_sender::set_loss_sequence(serv_parser.get_loss_sequence());
 
     set_mode(serv_parser.get_server_mode());
     std::cout << "Server listening on port: " << server::port_number << std::endl;
