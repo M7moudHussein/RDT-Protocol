b16f39ab6b57a0dfbfe74eff15af89266f1e4d7d
Ahmed Walid Kamal <ahmed3walid96@gmail.com>
2018-12-15 21:09:33 +0200

    Fixed mode setting of packet sender in server

--- server/server.cpp
+++ server/server.cpp
@@ -16,9 +16,10 @@ server::server(server_parser serv_parser) : MAX_WINDOW_SIZE(serv_parser.get_max_
 
     packet_sender::set_seed(serv_parser.get_random_seed());
     packet_sender::set_probability(serv_parser.get_loss_probability());
+    std::cout << serv_parser.get_loss_probability() << std::endl;
     packet_sender::set_mode(serv_parser.get_loss_mode());
     packet_sender::set_loss_sequence(serv_parser.get_loss_sequence());
-    packet_sender::set_mode(PROBABILITY);
+
     set_mode(serv_parser.get_server_mode());
     std::cout << "Server listening on port: " << server::port_number << std::endl;
     server::init();
